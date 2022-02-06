98f86087f3027c4cced9d24c11f3ebf03986edbe
cassnaze <cassandra.naze@marquette.edu>
2022-02-06 13:11:46 -0600

    Added logic into all functions in stats.ccp except for the histogram

--- Lab1/nazec_stats.cpp
+++ Lab1/nazec_stats.cpp
@@ -1,47 +1,75 @@
 /* Author: Cassie Naze
  * Created: 1 February 2022
  * This file calculates statistical data including min/max, mean, standard deviation and histogram values.
+ * Using comp apps lab 1 code for reference here
 */
 
 #include "nazec_stats.hpp"
 
 
-float BigCass::Statistics::calcMin(){
-    return this->data;
+float BigCass::Statistics::calcMin(std::vector<float>* data, int size){
+    float littlest = 5000; 
+    for(int i = 0; i < size; i++){
+        if(littlest > (*data)[i]){
+            littlest = (*data)[i];
+        }
+    }
+    return littlest;
 }
-void BigCass::Statistics::setMin(float data[]){
-
-    this->data = min;
-}
-
-float BigCass::Statistics::calcMax(){
-    return this->data;
-}
-void BigCass::Statistics::setMax(float data[]){
-
-    this->data = max;
+void BigCass::Statistics::setMin(float value){
+    this->min = value;
 }
 
-float BigCass::Statistics::calcMean(){
-    return this->data;
-}
-void BigCass::Statistics::setMean(float data[]){
 
-    this->data = mean;
+float BigCass::Statistics::calcMax(std::vector<float>* data, int size){
+    float biggest = 0;
+    for(int i = 0; i < size; i++){
+        if((*data)[i] > biggest){
+            biggest = (*data)[i];
+        }
+    }
+    return biggest; 
 }
-
-float BigCass::Statistics::calcStdDev(){
-    return this->data;
-}
-void BigCass::Statistics::setStdDev(float data[]){
-
-    this->data = std;
+void BigCass::Statistics::setMax(float value){
+    this->max = value;
 }
 
-float BigCass::Statistics::histogram(){
-    return this->data;
-}
-void BigCass::Statistics::setHistogram(float data[]){
 
-    this->data = bins[]; 
+float BigCass::Statistics::calcMean(std::vector<float>* data, int size){
+    float average; 
+    float sum = 0;
+
+    for(int i = 0; i < size; i++){
+        sum = sum + (*data)[i];
+    }
+    average = sum/size;
+
+    return average; 
+}
+void BigCass::Statistics::setMean(float value){
+    this->mean = value;
+}
+
+
+float BigCass::Statistics::calcStdDev(std::vector<float>* data, int size, float mean){
+    float variance;
+    float theStandard;
+    float sum = 0;
+
+    for(int i = 0; i < size; i++){
+        sum = sum + pow(((*data)[i] - mean), 2.0);
+    }
+
+    variance = sum/size;
+    theStandard = sqrt(variance);
+
+    return theStandard;
+}
+void BigCass::Statistics::setStdDev(float value){
+    this->std = value;
+}
+
+
+std::vector<float> BigCass::Statistics::histogram(std::vector<float>* data, int size, float mean, float std){
+
 }
--- Lab1/nazec_stats.hpp
+++ Lab1/nazec_stats.hpp
@@ -3,60 +3,75 @@
  * This file declares the functions for statistical analysis.
 */
 
-#ifndef stat
-#define stat
+#ifndef STATY
+#define STATY
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <math.h>
 
 namespace BigCass{
     class Statistics{
         public: 
-            Statistics();
+            //Statistics(); // Constructor.. do we need this?f
 
             /* This function is to calcuate the minimum value of the array
-            * @param data: Input array
+            * @param data: Input vector of the data
+            * @param size: Size of the input data vector
             * @returns: The min value 
             */ 
-            float calcMin();
-            void setMin(float data[]);
+            float calcMin(std::vector<float>* data, int size);
+            void setMin(float value);
 
             /* This function is to calcuate the max of the array
-            * @param data: Input array
+            * @param data: Input vector of the data
+            * @param size: Size of the input data vector
             * @returns: The max value
             */ 
-            float calcMax();
-            void setMax(float data[]);
+            float calcMax(std::vector<float>* data, int size);
+            void setMax(float value);
 
             /* This function is to calcuate the mean of input arrays
-            * @param data: Input array
+            * @param data: Input vector of the data
+            * @param size: Size of the input data vector
             * @returns: The mean of the input array
             */ 
-            float calcMean();
-            void setMean(float data[]);
+            float calcMean(std::vector<float>* data, int size);
+            void setMean(float value);
 
             /* This function is to calcuate the standard deviation of the input array
-            * @param data: Input array
+            * @param data: Input vector of the data
+            * @param size: Size of the input data vector
+            * @param mean: The mean calcuated from calcMean function
             * @returns: The standard deviation 
             */ 
-            float calcStdDev();
-            void setStdDev(float data[]);
+            float calcStdDev(std::vector<float>* data, int size, float mean);
+            void setStdDev(float value);
 
             /* This function is to estimate the histogram from input arguments
-            * @param data: Input array
-            * @returns: An array corresponding to the quantity of data in each bin
+            * @param data: Input vector of the data
+            * @param size: Size of the input data vector
+            * @param mean: The mean calcuated from calcMean function
+            * @param std: The standard deviation from calcStdDev function
+            * @returns: A vector corresponding to the quantity of data in each bin
+            * return hashbin, assign classvariables (vector or array that is the same length of elemetns in the bins) to get values in bins, calcualte bins externally to the 
+            * histogram and histogram function takes in the bin values
             */ 
-            float histogram(); 
-            void setHistogram(float data[]);
+            std::vector<float> histogram(std::vector<float>* data, int size, float mean, float std); 
 
         private: 
-            float data[]; // allocate it with something in the constructor; could look at std vectors 
-            std::vector<float> data;
-
-    }
+            //float data[]; // allocate it with something in the constructor; could look at std vectors 
+            //std::vector<float>* data; // this is actually going to be what we use as an arg for the functions
+            
+            float min;
+            float max;
+            float mean;
+            float std;
+            //float bins[];
+    };
 }
 
 #endif
