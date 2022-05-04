9b9a220b330014ece10a509f313b26500bdccbfe
cassnaze <cassandra.naze@marquette.edu>
2022-05-03 21:35:17 -0500

    Having troubles with some error that is saying no rile to make target

---
+++ midterm/midtermBeforeREvision.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Created by Cassie Naze
+# Midterm on 8 March 2022
+# Create a makefile
+# Script will take in a path to a directory with code files as an argument
+
+# Path for the test data /lab/bien4290/midterm2022/...
+
+# Check to see if the makefile already exists, if it does we dont have to make another one
+if [ ".nazec/bien4290/midterm/makefile" ]
+    then
+        echo "Makefile already exists"
+else
+    touch "makefile"
+fi
+
+# Check to see that this gets pushed and it worked hehe
+    # This will also not change regardless of what the rest does
+echo "# This is the makefile created for the midterm" > makefile
+echo "CC = g++" >> makefile
+
+# For the first trial of getting this done, I got stuck with input file stuff so I hardcored in some files
+# I think the logic would be similar but able to be used for an arbitrary amount of vals with making targets
+
+# 1. Generate separate targets for each cpp file in the directory
+    # nazec_stats.o: nazec_stats.cpp nazec_stats.hpp
+	# $(CC) -c $^
+
+# Hardcoding the files to my dir to be able to get something working
+#cp -r /lab/bien4290/midterm2022/Straightforward/. /home/nazec/bien4290/midterm/
+
+# Find where the .cpp files are and save those to an array
+readarray -t help < <(ls -l | grep ".cpp" | sed 's/[^ ]*[ ]//g')
+# echo $help # This prints only the first element
+
+readarray -t noRepeats < <(ls -l | grep ".cpp" | sed 's/[^ ]*[ ]//g' | sed 's/.cpp//g' )
+# echo $noRepeats
+
+#for i in "${noRepeats[@]}" # Tried to use this to iterate through the num of elements in the array
+for i in {1..3}
+do
+    echo ${noRepeats[$i]}.o: ${noRepeats[$i]}.cpp ${noRepeats[$i]}.hpp >> makefile
+    echo "	\$(CC) -c $^" >> makefile
+done
+
+# 2. Generate and "all" target which consists of each of the above targets and makes exe
+    # Determine where the main file is
+    # exe must be the name of the folder with the files
+    #grep -Fr "main"
+
+mainFile=$( ls -l | grep ".cpp" | grep -Fr "main" | cut -d ":" -f 1 | grep ".cpp" | sed 's/.cpp//g' )
+echo $mainFile
+
+echo "$mainFile.o: ${noRepeats[1]}.o ${noRepeats[3]}.o" >> makefile
+echo "	\$(CC) -c \$@ $^" >> makefile
+
+# 3. Generate a "clean" target that removes all relevant compilation files
+    # This should be correct regardlesss of the if the rest is hardcoded
+echo "clean:" >> makefile
+echo "	rm -f *.o" >> makefile
+
+# 4. Perform a make all and run the exe
+#make all
+echo "all: $mainFile.o ${noRepeats[1]}.o ${noRepeats[2]}.o ${noRepeats[3]}.o" >> makefile
+make clean
+
+# -----------------------------------------------
+# This is how we would do it from input argument
+# File input from the command line -> couldnt get this to work at first
+
+cd "$1"
+echo "New path is: '$(pwd)'" 
+
+# Ok so this part makes an array from the path that is input in the command
+readarray -t letsMakeThisWork < <(ls -l | grep ".cpp" | sed 's/[^ ]*[ ]//g' )
+echo $letsMakeThisWork
+
+readarray -t removeCPP < <(ls -l | grep ".cpp" | sed 's/[^ ]*[ ]//g' | sed 's/.cpp//g' )
+echo ${removeCPP}
+
+findMain=$( ls -l | grep ".cpp" | grep -Fr "main" | cut -d ":" -f 1 | grep ".cpp" | sed 's/.cpp//g' )
+echo $findMain
--- midterm/nazec_makey_mcmakerson.sh
+++ midterm/nazec_makey_mcmakerson.sh
@@ -6,75 +6,98 @@
 # Script will take in a path to a directory with code files as an argument
 
 # Path for the test data /lab/bien4290/midterm2022/...
+# MoreComplicated, Multimain, Simple, Straightforward
 
-if [ "./midterm/makefile" ]
+# Check to see if the makefile already exists, if it does we dont have to make another one
+if [ ".nazec/bien4290/midterm/makefile" ]
     then
         echo "Makefile already exists"
+        rm -r "makefile"
+        touch "makefile"
 else
     touch "makefile"
 fi
 
+newPath=$1
+
+# Error check the number of arguments entered 
+if [[ ($# -ne 1) ]]
+then
+    echo "Incorrect number of arguments entered"
+    exit 1
+fi
+
+# Error check if the directory exists before doing anything else
+if [ ! -e $newPath ]
+then   
+    echo "The path to $newPath does NOT exist"
+    exit 1
+fi
+
+# Otherwise, do stuff
+cd "$1"
+echo "New path is: $newPath"
+
 # Check to see that this gets pushed and it worked hehe
     # This will also not change regardless of what the rest does
-echo "# This is the makefile created for the midterm" > makefile
-echo "CC = g++" >> makefile
+echo "# This is the makefile created for the midterm" > /home/nazec/bien4290/midterm/makefile
+echo "CC = g++" >> /home/nazec/bien4290/midterm/makefile
 
-# For the first trial of getting this done, I got stuck with input file stuff so I hardcored in some files
-# I think the logic would be similar but able to be used for an arbitrary amount of vals with making targets
+# Save the file name to a variable
+nameOfFile=$(echo "$newPath" | sed "s/.*\///")
+
+# Ok so this part makes an array from the path that is input in the command
+readarray -t letsMakeThisWork < <(ls -l | grep ".cpp" | sed 's/[^ ]*[ ]//g' )
+echo ${letsMakeThisWork[1]}
+
 
 # 1. Generate separate targets for each cpp file in the directory
     # nazec_stats.o: nazec_stats.cpp nazec_stats.hpp
 	# $(CC) -c $^
-
-# Hardcoding the files to my dir to be able to get something working
-#cp -r /lab/bien4290/midterm2022/Straightforward/. /home/nazec/bien4290/midterm/
-
-# Find where the .cpp files are and save those to an array
-readarray -t help < <(ls -l | grep ".cpp" | sed 's/[^ ]*[ ]//g')
-# echo $help # This prints only the first element
-
-readarray -t noRepeats < <(ls -l | grep ".cpp" | sed 's/[^ ]*[ ]//g' | sed 's/.cpp//g' )
-# echo $noRepeats
-
-#for i in "${noRepeats[@]}" # Tried to use this to iterate through the num of elements in the array
-for i in {1..3}
-do
-    echo ${noRepeats[$i]}.o: ${noRepeats[$i]}.cpp ${noRepeats[$i]}.hpp >> makefile
-    echo "	\$(CC) -c $^" >> makefile
-done
-
 # 2. Generate and "all" target which consists of each of the above targets and makes exe
     # Determine where the main file is
     # exe must be the name of the folder with the files
     #grep -Fr "main"
-
 mainFile=$( ls -l | grep ".cpp" | grep -Fr "main" | cut -d ":" -f 1 | grep ".cpp" | sed 's/.cpp//g' )
-#echo $mainFile
+echo $mainFile
 
-echo "$mainFile.o: ${noRepeats[1]}.o ${noRepeats[3]}.o" >> makefile
-echo "	\$(CC) -c \$@ $^" >> makefile
+# Go through the array we made earlier to make the targets
+for (( i=0;i<${#letsMakeThisWork[@]};i++ ))
+do
+    # First look for the .cpp files that are not the one with the main
+    if [[ " ${letsMakeThisWork[i]} " =~ "cpp" &&  ${letsMakeThisWork[i]}  != $main ]]
+    then
+        echo -n "${letsMakeThisWork[$i]}" | sed 's/.cpp/.o:/g' >> /home/nazec/bien4290/midterm/makefile
+        makeTargets+=( $(echo -n "${letsMakeThisWork[$i]}" | sed 's/.cpp/.o/g') )
+        #echo ${makeTargets[i]}
+        echo -n "${letsMakeThisWork[$i]}"  >> /home/nazec/bien4290/midterm/makefile
+
+        echo -n " " >> /home/nazec/bien4290/midterm/makefile
+        echo "${letsMakeThisWork[$i]}" | sed 's/.cpp/.hpp/g' >> /home/nazec/bien4290/midterm/makefile
+        echo -e '\t$(CC) -c $^' >> /home/nazec/bien4290/midterm/makefile
+    # Look for the .cpp that are the main
+    elif  [[ ${letsMakeThisWork[i]}  == $mainFile ]]
+    then
+        makeTargets+=( $(echo -n "${letsMakeThisWork[$i]}") )
+    fi
+    
+
+done
+
+
+echo "$nameOfFile.o:${makeTargets[*]}" >> /home/nazec/bien4290/midterm/makefile
+echo -e '\t$(CC) -c $@ $^' >> /home/nazec/bien4290/midterm/makefile
+
+echo "all:$nameOfFile.o" >> /home/nazec/bien4290/midterm/makefile
 
 # 3. Generate a "clean" target that removes all relevant compilation files
     # This should be correct regardlesss of the if the rest is hardcoded
-echo "clean:" >> makefile
-echo "	rm -f *.o" >> makefile
+echo "clean:" >> /home/nazec/bien4290/midterm/makefile
+echo -e "\trm -f *.o" >> /home/nazec/bien4290/midterm/makefile
+echo -e "\trm -f *.gch" >> /home/nazec/bien4290/midterm/makefile
 
 # 4. Perform a make all and run the exe
 #make all
-echo "all: $mainFile.o ${noRepeats[1]}.o ${noRepeats[2]}.o ${noRepeats[3]}.o" >> makefile
-
-# -----------------------------------------------
-# This is how we would do it from input argument
-# File input from the command line -> couldnt get this to work at first
-cd "$1"
-echo "New path is: '$(pwd)'" 
-
-# Ok so this part makes an array from the path that is input in the command
-readarray -t letsMakeThisWork < <(ls -l | grep ".cpp" | sed 's/[^ ]*[ ]//g' )
-echo $letsMakeThisWork
-
-readarray -t removeCPP < <(ls -l | grep ".cpp" | sed 's/[^ ]*[ ]//g' | sed 's/.cpp//g' )
-echo ${removeCPP}
-
-findMain=$( ls -l | grep ".cpp" | grep -Fr "main" | cut -d ":" -f 1 | grep ".cpp" | sed 's/.cpp//g' )
-echo $findMain
+#make clean
+cd /home/nazec/bien4290/midterm/
+make all
